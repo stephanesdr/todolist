03ea2d2a984410a926fe6455ba8cd3cbb9d12df4
stephanesdr <stadlerstephane@gmail.com>
2019-02-28 12:20:13 +0100

    asset test

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module TodolistApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
